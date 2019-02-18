a7549c0988f6dbfc8dc7d964672b837a63ce2c2a
andanfe@gmail.com <andanfe@gmail.com>
2019-02-18 14:02:11 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((request, response, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${request.method} ${request.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (error) => {
+    if(error) {
+      console.log('unable to append to server.log');
+    }
+  });
+
+  next();
+});
+
+// app.use((request, response, next) => {
+//   response.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+  response.render('home.hbs', {
+    pageTitle: 'welcome to main page',
+    welcomeMessage: 'Nu darova stalker'
+  });
+});
+
+app.get('/about', (request, response) => {
+  response.render('about.hbs', {
+    pageTitle: 'about page'
+  });
+});
+
+app.get('/bad', (request, response) => {
+  response.send({
+    errorMessage: 'something bad has occurred'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('server is up on port 3000');
+});
