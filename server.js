f2a5319651c0d8cf30784998b281e94269b2f980
andanfe@gmail.com <andanfe@gmail.com>
2019-02-18 15:23:58 +0200

    added portfolio page

--- server.js
+++ server.js
@@ -51,6 +51,12 @@ app.get('/about', (request, response) => {
   });
 });
 
+app.get('/projects', (request, response) => {
+  response.render('projects.hbs', {
+    pageTitle: 'projects page'
+  });
+});
+
 app.get('/bad', (request, response) => {
   response.send({
     errorMessage: 'something bad has occurred'
